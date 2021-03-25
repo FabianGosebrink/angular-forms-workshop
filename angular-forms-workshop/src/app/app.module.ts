001a9801bf4c89e77f7438e077138e6d0d59b8d5
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-03-25 08:41:48 +0100

    removed all files which are gonna be live coded

--- angular-forms-workshop/src/app/app.module.ts
+++ angular-forms-workshop/src/app/app.module.ts
@@ -5,24 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { BlockButtonsComponent } from './block-buttons/block-buttons.component';
-import { FormSimpleArrayComponent } from './form-simple-array/form-simple-array.component';
-import { FormSimpleControlComponent } from './form-simple-control/form-simple-control.component';
-import { FormSimpleGroupComponent } from './form-simple-group/form-simple-group.component';
 import { MaterialModule } from './material/material.module';
 import { NavigationComponent } from './navigation/navigation.component';
-import { FormSimpleSearchComponent } from './form-simple-search/form-simple-search.component';
 
 @NgModule({
-  declarations: [
-    NavigationComponent,
-    AppComponent,
-    FormSimpleControlComponent,
-    FormSimpleGroupComponent,
-    BlockButtonsComponent,
-    FormSimpleArrayComponent,
-    FormSimpleSearchComponent,
-  ],
+  declarations: [NavigationComponent, AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
