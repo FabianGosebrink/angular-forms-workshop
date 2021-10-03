2454761f83e245817b433fdbe652a7719931f0f0
FabianGosebrink <fabian.gosebrink@outlook.com>
2021-10-03 17:28:57 +0200

    Fixes small errors

--- angular-forms-workshop/src/app/form-simple-control/form-simple-control.component.ts
+++ angular-forms-workshop/src/app/form-simple-control/form-simple-control.component.ts
@@ -1,25 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
+import { FormControl, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from './../services/error-state-matcher';
 
 @Component({
   selector: 'app-form-simple-control',
