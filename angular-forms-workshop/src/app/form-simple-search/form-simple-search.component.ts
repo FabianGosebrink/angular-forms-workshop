b482266e8fcc10ee0d385f0fb65beb6e3c4b70ce
FabianGosebrink <fabian.gosebrink@outlook.com>
2022-10-19 18:30:05 +0200

    Added Object when working with the search

--- angular-forms-workshop/src/app/form-simple-search/form-simple-search.component.ts
+++ angular-forms-workshop/src/app/form-simple-search/form-simple-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Observable, of } from 'rxjs';
 import {
   catchError,
@@ -12,14 +13,19 @@ import {
 } from 'rxjs/operators';
 import { HttpService } from '../services/http.service';
 
+export interface NpmResponse {
+  name: string;
+  latest: string;
+}
+
 @Component({
   selector: 'app-form-simple-search',
   templateUrl: './form-simple-search.component.html',
   styleUrls: ['./form-simple-search.component.css'],
 })
 export class FormSimpleSearchComponent implements OnInit {
-  myControl = new FormControl<string>('');
-  filteredOptions: Observable<string[]>;
+  myControl = new FormControl<NpmResponse>(null);
+  filteredOptions: Observable<NpmResponse[]>;
 
   constructor(private http: HttpService) {}
 
@@ -33,14 +39,23 @@ export class FormSimpleSearchComponent implements OnInit {
         this.http.get(`https://api.cdnjs.com/libraries?search=${value}`)
       ),
       map((result: any) => {
-        const allResults = result.results as any[];
+        const allResults = result.results as NpmResponse[];
 
         if (!allResults.length) {
           return [];
         }
-        return allResults.map((x) => x.name);
+
+        return allResults;
       }),
       catchError(() => of([]))
     );
+  }
+
+  displayFn(response: NpmResponse): string {
+    return response?.name ? response.name : '';
+  }
+
+  optionSelected(event: MatAutocompleteSelectedEvent) {
+    console.log(event.option.value);
   }
 }
